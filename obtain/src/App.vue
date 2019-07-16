3ba687931a1a1c85888a228d1f0797406c71773e
WangLiXiao <15029021831@163.com>
2019-07-16 16:28:16 +0800

    wlx上传

---
+++ obtain/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body,p,ul,ol,dl,dd,h1,h2,h3,h4,h5,h6,td,input,textarea,select,option{
+	margin: 0;
+	padding: 0;
+}
+
+html{
+	font-size: 26.67vw;
+}
+
+html,body{
+	height: 100%;
+}
+body{
+	font-family: "微软雅黑";
+}
+ul,ol,li{
+	list-style:none;
+}
+a{
+	text-decoration: none;
+}
+
+img{
+	display: block;
+}
+</style>
---
+++ obtain/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../pages/home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path:'/*',
+      redirect:'/home'
+    }
+  ]
+})
